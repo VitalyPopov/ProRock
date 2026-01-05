331f6fc5c16c99574d1830db9af7d1dd192de971
Vitaly Popov <48799812+VitalyPopov@users.noreply.github.com>
2026-01-05 13:36:13 +0400

    Samples: align Xmlite namespaces demo with basic demo structure

--- Samples/ProRock.Xmlite.Namespaces/ProRock.Xmlite.Schema.urndemoorder.pas
+++ Samples/ProRock.Xmlite.Namespaces/ProRock.Xmlite.Schema.urndemoorder.pas
@@ -1,8 +1,8 @@
 ﻿unit ProRock.Xmlite.Schema.urndemoorder;
 
 (*
-    This unit was automatically generated using ProRocket 1.0.0 Lite (ProRock 1.0.0)
-    Generated (UTC): 2026-01-04T10:21:03.820Z
+    This unit was automatically generated using ProRocket 1.0.1 Lite (ProRock 1.0.0)
+    Generated (UTC): 2026-01-05T09:26:24.322Z
     Namespace: urn:demo:order
     
     ProRock is a free and open-source Delphi library. Feedback and contributions are welcome.
@@ -18,52 +18,53 @@ uses
 type
   TOrderStatusST = (osNew, osProcessing, osShipped, osCompleted, osCancelled);
 
-  TCurrencyCodeST = type TStringST;
+  TCurrencyCodeST = type ProRock.Xmlite.Schema.Base.TStringST;
+  TPriceST = type ProRock.Xmlite.Schema.Base.TDecimalST;
+  TQuantityST = type ProRock.Xmlite.Schema.Base.TIntST;
 
-  TMoneyTypeCT = class;
   TCustomerTypeCT = class;
   TItemTypeCT = class;
   TItemsTypeCT = class;
+  TMoneyTypeCT = class;
   TOrderE = class;
 
-  TMoneyTypeCTList = class;
   TCustomerTypeCTList = class;
   TItemTypeCTList = class;
   TItemsTypeCTList = class;
+  TMoneyTypeCTList = class;
   TOrderEList = class;
-
-  TMoneyTypeCT = class(TXmliteComplexType)
-  private
-    fAmount: TDecimalST;
-    fCurrency: TCurrencyCodeST;
-  published
-    property Amount: TDecimalST read fAmount write fAmount;
-    property Currency: TCurrencyCodeST read fCurrency write fCurrency;
-  end;
 
   TCustomerTypeCT = class(TXmliteComplexType)
   private
-    fId: TStringST;
-    fName: TStringST;
-    fEmail: TStringST;
+    fId: ProRock.Xmlite.Schema.Base.TStringST;
+    fName: ProRock.Xmlite.Schema.Base.TStringST;
+    fEmail: ProRock.Xmlite.Schema.Base.TStringST;
   published
-    property Id: TStringST read fId write fId;
-    property Name: TStringST read fName write fName;
-    property Email: TStringST read fEmail write fEmail;
+    [TXmliteElement]
+    property Id: ProRock.Xmlite.Schema.Base.TStringST read fId write fId;
+    [TXmliteElement]
+    property Name: ProRock.Xmlite.Schema.Base.TStringST read fName write fName;
+    [TXmliteElement]
+    property Email: ProRock.Xmlite.Schema.Base.TStringST read fEmail write fEmail;
   end;
 
   TItemTypeCT = class(TXmliteComplexType)
   private
-    fSku: TStringST;
-    fQuantity: TIntST;
-    fTitle: TStringST;
-    fPrice: TDecimalST;
+    fSku: ProRock.Xmlite.Schema.Base.TStringST;
+    fTitle: ProRock.Xmlite.Schema.Base.TStringST;
+    fQuantity: TQuantityST;
+    fPrice: TPriceST;
     fCurrency: TCurrencyCodeST;
   published
-    property Sku: TStringST read fSku write fSku;
-    property Quantity: TIntST read fQuantity write fQuantity;
-    property Title: TStringST read fTitle write fTitle;
-    property Price: TDecimalST read fPrice write fPrice;
+    [TXmliteElement]
+    property Sku: ProRock.Xmlite.Schema.Base.TStringST read fSku write fSku;
+    [TXmliteElement]
+    property Title: ProRock.Xmlite.Schema.Base.TStringST read fTitle write fTitle;
+    [TXmliteElement]
+    property Quantity: TQuantityST read fQuantity write fQuantity;
+    [TXmliteElement]
+    property Price: TPriceST read fPrice write fPrice;
+    [TXmliteElement]
     property Currency: TCurrencyCodeST read fCurrency write fCurrency;
   end;
 
@@ -74,35 +75,49 @@ type
     property Item: TItemTypeCTList read fItem;
   end;
 
-  [TNamingAttribute(TNaming.nFlatCase)]
+  TMoneyTypeCT = class(TXmliteComplexType)
+  private
+    fAmount: ProRock.Xmlite.Schema.Base.TDecimalST;
+    fCurrency: TCurrencyCodeST;
+  published
+    [TXmliteElement]
+    property Amount: ProRock.Xmlite.Schema.Base.TDecimalST read fAmount write fAmount;
+    [TXmliteElement]
+    property Currency: TCurrencyCodeST read fCurrency write fCurrency;
+  end;
+
+  [TNaming(TNaming.nFlatCase)]
   TOrderE = class(TXmliteElement)
   private
-    fId: TStringST;
-    fDate: TDateTimeST;
+    fId: ProRock.Xmlite.Schema.Base.TStringST;
+    fDate: ProRock.Xmlite.Schema.Base.TDateTimeST;
     fStatus: TOrderStatusST;
     fCustomer: TCustomerTypeCT;
     fItems: TItemsTypeCT;
     fTotal: TMoneyTypeCT;
   published
-    property Id: TStringST read fId write fId;
-    property Date: TDateTimeST read fDate write fDate;
+    [TXmliteElement]
+    property Id: ProRock.Xmlite.Schema.Base.TStringST read fId write fId;
+    [TXmliteElement]
+    property Date: ProRock.Xmlite.Schema.Base.TDateTimeST read fDate write fDate;
+    [TXmliteElement]
     property Status: TOrderStatusST read fStatus write fStatus;
     property Customer: TCustomerTypeCT read fCustomer;
     property Items: TItemsTypeCT read fItems;
     property Total: TMoneyTypeCT read fTotal;
   end;
 
-  TMoneyTypeCTList = class(TXmliteList<TMoneyTypeCT>);
   TCustomerTypeCTList = class(TXmliteList<TCustomerTypeCT>);
   TItemTypeCTList = class(TXmliteList<TItemTypeCT>);
   TItemsTypeCTList = class(TXmliteList<TItemsTypeCT>);
+  TMoneyTypeCTList = class(TXmliteList<TMoneyTypeCT>);
   TOrderEList = class(TXmliteList<TOrderE>);
 
 implementation
 
 initialization
 
-TMetaBankXmlite.RegisterNamespace('urn:demo:order', [TypeInfo(TOrderStatusST), TypeInfo(TCurrencyCodeST)], [TMoneyTypeCT, TCustomerTypeCT,
-  TItemTypeCT, TItemsTypeCT], [], [], [TOrderE], [], 'o', TNaming.nPascalCase);
+TMetaBankXmlite.RegisterNamespace('urn:demo:order', [TypeInfo(TOrderStatusST), TypeInfo(TCurrencyCodeST), TypeInfo(TQuantityST),
+  TypeInfo(TPriceST)], [TCustomerTypeCT, TItemTypeCT, TItemsTypeCT, TMoneyTypeCT], [], [], [TOrderE], [], 'o', TNaming.nPascalCase);
 
 end.
